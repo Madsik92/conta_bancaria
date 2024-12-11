bc117a38aa890d5fbadacb99149ffab563575477
Jessica Rosário <jessica.rosario4@gmail.com>
2024-12-11 12:25:17 -0300

    :sparkles: feat: Classe Model Conta

--- Menu.ts
+++ Menu.ts
@@ -1,81 +1,99 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Colors"
+import { Conta } from "./src/model/Conta";
 
-let opcao: number;
+export function main() {
 
-do {
-    menu()
+    let opcao: number;
 
-    opcao = readlinesync.questionInt();
+    // Cria novas instancias (objetos) da classe conta
+    const c1 = new Conta(1, 123, 1, "Jonas", 100000);
+    c1.visualizar();
+    // saque
+    console.log(c1.sacar(100.00));
+    c1.visualizar();
 
-    if (opcao === 0) {
-        about()
-        process.exit(0)
-    }
+    const c2 = new Conta(2, 123, 2, "Aline", 200000);
+    c2.visualizar();
+    // deposito
+    c2.depositar(100.00);
+    c2.visualizar();
 
+    do {
+        menu()
 
-    switch (opcao) {
-        case 1:
-            console.log('Criar Conta');
-            break;
+        opcao = readlinesync.questionInt();
 
-        case 2:
-            console.log('Listar todas as Contas');
-            break;
-
-        case 3:
-            console.log('Buscar Conta por Numero');
-            break;
-
-        case 4:
-            console.log('Atualizar Dados da Conta');
-            break;
-
-        case 5:
-            console.log('Apagar Conta');
-            break;
-
-        case 6:
-            console.log('Sacar');
-            break;
-
-        case 7:
-            console.log('Depositar');
-            break;
-
-        case 8:
-            console.log('Transferir valores entre Contas');
-            break;
-
-        default:
-            console.log("Opcao Invalida")
+        if (opcao == 9) {
+            about()
+            process.exit(0)
+        }
 
 
-    }
+        switch (opcao) {
+            case 1:
+                console.log('Criar Conta');
+                break;
 
-} while (true);
+            case 2:
+                console.log('Listar todas as Contas');
+                break;
 
+            case 3:
+                console.log('Buscar Conta por Numero');
+                break;
+
+            case 4:
+                console.log('Atualizar Dados da Conta');
+                break;
+
+            case 5:
+                console.log('Apagar Conta');
+                break;
+
+            case 6:
+                console.log('Sacar');
+                break;
+
+            case 7:
+                console.log('Depositar');
+                break;
+
+            case 8:
+                console.log('Transferir valores entre Contas');
+                break;
+
+            default:
+                console.log("Opcao Invalida")
+
+
+        }
+
+    } while (true);
+}
 
 function menu(): void {
 
-    console.log(colors.bg.greenbright,colors.fg.blackstrong, "=====================================================  ");
-    console.log("                       FOLK'OR BANK                      ");
-    console.log("  ----------------------------------------------------- ", colors.reset);
-    console.log(colors.fg.greenstrong,"                                                       ");
-    console.log("            1 - Criar Conta                              ");
-    console.log("            2 - Listar todas as Contas                   ");
-    console.log("            3 - Buscar conta por Numero                  ");
-    console.log("            4 - Atualizar Dados da Conta                 ");
-    console.log("            5 - Apagar Conta                             ");
-    console.log("            6 - Sacar                                    ");
-    console.log("            7 - Depositar                                ");
-    console.log("            8 - Transferir valores entre Contas          ");
-    console.log("            9 - Sair                                     ");
-    console.log("                                                         ");
-    console.log("  ===================================================== ", colors.reset);
-    console.log("\n  Entre com a opcao desejada:");
+    console.log(colors.fg.greenstrong,"=====================================================");
+    console.log("                    FOLK'OR BANK                      ");
+    console.log(" -----------------------------------------------------");
+    console.log("                                                      ");
+    console.log("        1 - Criar Conta                               ");
+    console.log("        2 - Listar todas as Contas                    ");
+    console.log("        3 - Buscar conta por Numero                   ");
+    console.log("        4 - Atualizar Dados da Conta                  ");
+    console.log("        5 - Apagar Conta                              ");
+    console.log("        6 - Sacar                                     ");
+    console.log("        7 - Depositar                                 ");
+    console.log("        8 - Transferir valores entre Contas           ");
+    console.log("        9 - Sair                                      ");
+    console.log("                                                      ");
+    console.log(" =====================================================");
+    console.log("\n Entre com a opcao desejada:", colors.reset);
 }
 
 function about() {
     console.log("Criado por Jessica")
 }
+
+main();
