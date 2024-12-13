4fe9379c3c2ade88a9e006fd31d2dc2b7832c596
Jessica Rosário <jessica.rosario4@gmail.com>
2024-12-13 10:47:54 -0300

    Criacao da Interface Repository

--- Menu.ts
+++ Menu.ts
@@ -1,50 +1,11 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Colors"
-import { Conta } from "./src/model/Conta";
 import { ContaCorrente } from "./src/model/ContaCorrente";
 import { ContaPoupanca } from "./src/model/ContaPoupanca";
 
 export function main() {
 
     let opcao: number;
-
-    // // Cria novas instancias (objetos) da classe conta
-    // const c1 = new Conta(1, 123, 1, "Jonas", 100000);
-    // c1.visualizar();
-    // // saque
-    // console.log(c1.sacar(100.00));
-    // c1.visualizar();
-
-    // const c2 = new Conta(2, 123, 2, "Aline", 200000);
-    // c2.visualizar();
-    // // deposito
-    // c2.depositar(100.00);
-    // c2.visualizar();
-
-
-    // Contas Correntes
-    const cc1 = new ContaCorrente(3, 789, 1, "Andressa", 100000, 1000);
-    cc1.visualizar();
-
-    // Saque na Conta Corrente
-    cc1.sacar(100500);
-    cc1.visualizar();
-
-    // Deposito na Conta Corrente
-    cc1.depositar(2000);
-    cc1.visualizar();
-
-    // Deposito e Saque na Conta Poupanca
-    const cp1 = new ContaPoupanca(4, 474, 2, "Jessica", 1000, 12);
-    cp1.visualizar();
-    cp1.sacar(1001);
-    cp1.visualizar();
-    cp1.sacar(500);
-    cp1.visualizar();
-    cp1.depositar(300);
-    cp1.visualizar();
-
-
 
     while (true) {
         menu()
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,4 +1,4 @@
-export class Conta{
+export abstract class Conta{
 
     // Definir modelo de dados (atributos)
     private _numero: number;
